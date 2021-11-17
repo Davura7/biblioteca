014fa9943738f6aba808143f2afcda36e09662c4
TurboBoy090 <93775732+TurboBoy090@users.noreply.github.com>
2021-11-17 13:46:43 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -5,11 +5,26 @@ datosIniciales();
 
 function datosIniciales() {
   oBiblioteca.catalogo = [
-    { idArticulo: 1, nombre: "El Quijote", prestado: false },
+    new Libro("Hugo", 43, 18, "El Quijote", false),
     { idArticulo: 2, nombre: "El Quijote II", prestado: false },
     { idArticulo: 3, nombre: "El Quijote III", prestado: true },
+    new DVD("2020-03-23", true, 33, "el rey leon", false),
     { idArticulo: 4, nombre: "El Quijote IV", prestado: false },
     { idArticulo: 5, nombre: "El Quijote V", prestado: false }
+  ];
+  oBiblioteca.usuarios = [
+    { idUsuario: 1, nombre: "juan1", apellidos: "gonzalez1", telefono: "424242424" },
+    { idUsuario: 2, nombre: "juan2", apellidos: "gonzalez2", telefono: "424242424" },
+    { idUsuario: 3, nombre: "juan3", apellidos: "gonzalez3", telefono: "424242424" },
+    { idUsuario: 4, nombre: "juan4", apellidos: "gonzalez4", telefono: "424242424" }
+  ]
+  oBiblioteca.prestamos = [
+    //idPrestamo, articulos, usuario, fechaInicio, fechaFin
+    {
+      idPrestamo: 1, articulos: [{ idArticulo: 1, nombre: "El Quijote", prestado: false }],
+      usuario: { idUsuario: 1, nombre: "juan1", apellidos: "gonzalez1", telefono: "424242424" },
+      fechaInicio: new Date("2021-03-4"), fechaFin: null
+    }
   ];
 }
 
@@ -29,6 +44,12 @@ function gestionFormularios(sFormularioVisible) {
       break;
     case "frmAltaPrestamo":
       frmAltaPrestamo.style.display = "block";
+
+
+      frmAltaPrestamo.lstLibros1.innerHTML = oBiblioteca.optionsLibros();
+      frmAltaPrestamo.lstDVD1.innerHTML = oBiblioteca.optionsDVD();
+      frmAltaPrestamo.lstLibros2.innerHTML = oBiblioteca.optionsLibros();
+      frmAltaPrestamo.lstDVD2.innerHTML = oBiblioteca.optionsDVD();
       break;
     case "frmDevolverPrestamo":
       frmDevolverPrestamo.style.display = "block";
@@ -36,9 +57,6 @@ function gestionFormularios(sFormularioVisible) {
       frmAltaPrestamo.lstLibros2.innerHTML = frmAltaPrestamo.lstLibros1.innerHTML;
       frmAltaPrestamo.lstDVD1.innerHTML = oBiblioteca.optionsDVD();
       frmAltaPrestamo.lstDVD2.innerHTML = frmAltaPrestamo.lstDVD1.innerHTML;
-      break;
-    case "TotalArboles":
-      alert("Hay " + oVivero.totalArbolesVenta() + " árboles a la venta");
       break;
   }
 }
@@ -62,7 +80,7 @@ function validarDatosUsuario() {
   let apellidos = frmAltaUsuario.txtApellidos.value;
   let telefono = frmAltaUsuario.txtTelefono.value;
 
-  if (isNaN(parseInt(idUsuario))||
+  if (isNaN(parseInt(idUsuario)) ||
     nombre == '' ||
     apellidos == '' ||
     isNaN(parseInt(telefono)))
@@ -100,11 +118,12 @@ function validarDatosArticulo() {
   let titulo = frmAltaArticulo.txtTitulo.value;
   let autor = frmAltaArticulo.txtAutor.value;
   let paginas = frmAltaArticulo.txtPaginas.value;
+  let fechaEstreno = frmAltaArticulo.fechaEstreno;
   let oArticulo
 
   if (document.querySelector("#rbtTipoArticulo-L").checked) {
 
-    if (isNaN(idArticulo) ||
+    if (isNaN(parseInt(idArticulo)) ||
       titulo == '' ||
       autor == '' ||
       isNaN(parseInt(paginas))) {
@@ -120,24 +139,24 @@ function validarDatosArticulo() {
       alert(oBiblioteca.altaArticulo(oArticulo));
 
     }
+  }
+  
+  else {
 
-    if (!document.querySelector("#rbtTipoArticulo-L").checked) {
+    if (isNaN(parseInt(idArticulo)) ||
+      titulo == '' ||
+      autor == '' ||
+      fechaEstreno == '') {
 
-      if (idArticulo == '' ||
-        titulo == '' ||
-        autor == '' ||
-        fechaEstreno == '') {
-
-        alert('Debe introducir todos los datos');
-
-      }
+      alert('Debe introducir todos los datos correctamente');
 
     }
+
+
 
     else {
 
       let subtitulada = document.querySelector("#rbtSubtitulado-S").checked;
-
       oArticulo = new DVD(new Date(fechaEstreno), subtitulada, idArticulo, titulo);
 
       alert(oBiblioteca.altaArticulo(oArticulo));
@@ -148,5 +167,20 @@ function validarDatosArticulo() {
 }
 
 
+//Ejercicio 3
+//Un usuario sólo podrá tener un préstamo en vigor. No podrá solicitar otro préstamo hasta que realice la devolución del anterior.
 
 
+function validarDatosPrestamo() {
+
+  //let idUsuario =  document.getElementById("txtIdUsuario").value;
+  //let resultado =oBiblioteca.altaPrestamo();
+  //let txtIdPrestamo = frmAltaUsuario.txtIdPrestamo.value;
+  //let nombre = frmAltaUsuario.txtNombre.value;
+  //let apellidos = frmAltaUsuario.txtApellidos.value;
+  //let telefono = frmAltaUsuario.txtTelefono.value;
+
+
+
+  //lstLibros1
+}
